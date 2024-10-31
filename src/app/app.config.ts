8b401553bb5cca620f5f3881012e58ef51fbdf48
Venyamin Shvets <venyashvets@gmail.com>
2024-10-31 20:53:56 +0300

    add angular material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,13 @@ import {
   provideZoneChangeDetection,
 } from "@angular/core";
 import { provideRouter } from "@angular/router";
-
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideAnimationsAsync(),
   ],
 };
